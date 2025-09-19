de1cf997afad083cef10e8291a35af2f8c3055da
absajallow <absajallowjay@gmail.com>
2025-09-19 00:34:38 -0500

    week 4

---
+++ Week4/week4.py
@@ -0,0 +1,33 @@
+
+#absa jay
+'''
+larger number =int(input("Enter the larger number:"))
+smallest number = int(input("Enter the larger number"))
+num=0
+while larger_num > smallest num
+    larger_num /=2
+    num += 1
+    print(f"Number od the times haived: {num}")
+'''
+ #question 2
+
+word =input("Enter a word: ")
+result =""
+for 1 in range(1, len(word) -1,2):
+   result += word(1)
+   print(f"output =(result)")
+
+   #    question 4
+   while true:
+       user_in = input("Enter a letter: ")
+       if user_in == "done":
+           break
+       else:
+           word += user_in
+       print(f"The final word is {word}")    
+
+       #question 5
+       start =50
+       end=517
+    for i in range (start + 1,end + 1,2):
+       print{1}
